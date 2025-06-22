3175170471ad5b8e6a9f17440bd477e0e01455cf
shoaibwaqar665 <shoaibwaqar@weteck.co>
2025-06-23 02:00:35 +0500

    urls

--- blog_source.py
+++ blog_source.py
@@ -3,7 +3,11 @@ from bs4 import BeautifulSoup
 
 
 # url = "https://techpoint.africa/"
-url = "https://technext.ng/"
+# url = "https://technext.ng/"
+# url='https://cioafrica.co/'
+# url='https://techmoran.com/'
+# url='https://ventureburn.com/'
+url='https://www.itnewsafrica.com/'
 # # add headers to the request
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
@@ -33,7 +37,7 @@ with open('links.txt', 'r') as f:
         '/contact', '/social', '/facebook', '/twitter', '/instagram', '/linkedin', 
         '/youtube', '/pinterest', '/tiktok', '/snapchat', '/reddit', '/medium', 
         '/quora', '/github', '/subject/', '/about/', '/privacy/', '/terms/', 
-        '/disclaimer/', '/advertise/', '/brand-press', '/digest', '/newsletters/','/about-us','/category/','#','/tags','/author/','/tag/','privacy-policy','www.instagram.com','www.youtube.com','www.facebook.com','www.linkedin.com','www.twitter.com','www.tiktok.com','www.snapchat.com','www.reddit.com','www.medium.com','www.quora.com','www.github.com','/reach-out/','/rss.com/','/coinference','https://events.','https://technext24.com/fleshly-pressed/','https://technext24.com/technext-ng-media-privacy','https://x.com','https://techpoint.africa/editorial-team/'
+        '/disclaimer/', '/advertise/', '/brand-press', '/digest', '/newsletters/','/about-us','/category/','#','/tags','/author/','/tag/','privacy-policy','www.instagram.com','www.youtube.com','www.facebook.com','www.linkedin.com','www.twitter.com','www.tiktok.com','www.snapchat.com','www.reddit.com','www.medium.com','www.quora.com','www.github.com','/reach-out/','/rss.com/','/coinference','https://events.','https://technext24.com/fleshly-pressed/','https://technext24.com/technext-ng-media-privacy','https://x.com','https://techpoint.africa/editorial-team/','newsletter','/newsletter','https://web.facebook.com/TechpointAfrica','/categories','https://techcabal.com/latest','https://techcabal.com/events','http://insights.techcabal.com/','https://techcabal.com/standards-and-policies/','http://techwomenlagos.com','https://insights.techcabal.com/reports/','https://cioafrica.co/contact-us/','https://cioafrica.co/terms-of-service/','https://cioafrica.co/privacy-policy/','https://techcabal.com/?page_id=89385','https://publications.cioafrica.co/2025/June-WA','https://cioafrica.co/advertising/','https://nohitsradio.live/','javascript:void(0);','https://cioafrica.co/wp-login.php','https://events.cioafrica.co','https://cioafrica.co/southern-africa/','https://cioafrica.co/services/','https://techmoran.com/?page_id=191562','http://888starz.co.ke/en','https://publications.cioafrica.co','https://cioafrica.co/west-africa/','https://techmoran.com/technight/','/advertise','/about','category/startup-news','.net/','/companies/suppliedcontent','mailto:nomthandazo.mhlanga@memeburn.com','/companies/esquared'
     ]
     
     # filter out links containing any unwanted patterns
